f40452a4b5f6d36e1d4e90aa5d97c6659d38c591
DeadKesar <DeadK@DESKTOP-6J5VCO6>
2021-10-22 15:55:35 +0300

    Добавьте файлы проекта.

---
+++ enc_temp_folder/a1df31e9ca5d76772fbf8b3a7ed12310/лаб 2.cpp
@@ -0,0 +1,57 @@
+﻿
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+string GetText();
+
+int main()
+{            
+    string testText = "aa sds ss sssss a a a a a a a a a 6789876";
+    
+   // getline(cin, testText);
+    
+    string longestWord;
+    if (testText.length() > 0)
+    {
+        longestWord = testText.substr(0, 1);
+    }
+    else
+    {
+        //определить поведения при пустом вводе.
+    }
+    int maxLength = 1;
+    for (int i = 0; i < testText.length()-1; i++)
+    {
+        for (int j = 2; j < testText.length()-i; j++)
+        {
+            if (testText[i+j-1] == ' ')
+            {
+                i += j - 1;
+                break;
+            }
+            string sub = testText.substr(i, j);
+            string reSub = sub;
+            reverse(reSub.begin(), reSub.end());
+            if (sub == reSub && sub.length() > maxLength)
+            {
+                longestWord = sub;
+                maxLength = sub.length();
+            }
+        }
+    }
+    cout << longestWord << "    " << maxLength << endl;
+    return 0;
+}
+
+string GetText()
+{
+    string text;
+    getline(cin, text);
+    return "";
+}
+
+
+
---
+++ лаб 2/лаб 2.cpp
@@ -0,0 +1,65 @@
+﻿
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+string GetText();
+string FindLongestWord(string text);
+
+int main()
+{            
+    string testText = "aa sds ss sssss a a a a a a a a a 6789876";
+    
+   // getline(cin, testText);
+    
+    string answer = FindLongestWord(testText);
+    cout << answer << endl;
+    return 0;
+}
+
+string FindLongestWord(string text)
+{
+    string longestWord;
+    if (text.length() > 0)
+    {
+        longestWord = text.substr(0, 1);
+    }
+    else
+    {
+        //определить поведения при пустом вводе.
+    }
+    int maxLength = 1;
+    for (int i = 0; i < text.length() - 1; i++)
+    {
+        for (int j = 1; j < text.length() - i; j++)
+        {
+            if (text[i + j - 1] == ' ' || text[i + j - 1] == '.' || text[i + j - 1] == '!', text[i + j - 1] == '?')
+                //  поиск среди осмысленных слов. Мы пологаем что слова отделены друг от друга разделителями.
+            {
+                i += j - 1;
+                break;
+            }
+            string sub = text.substr(i, j);
+            string reSub = sub;
+            reverse(reSub.begin(), reSub.end());
+            if (sub == reSub && sub.length() > maxLength)
+            {
+                longestWord = sub;
+                maxLength = sub.length();
+            }
+        }
+    }
+    return longestWord;
+}
+
+string GetText()
+{
+    string text;
+    getline(cin, text);
+    return "";
+}
+
+
+
